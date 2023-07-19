1e19b9b776d3f8574233fc8f920eae99706a9004
RochaMoran <rochamoran1@gmail.com>
2023-07-18 20:48:09 -0600

    REFACTOR: health section

--- src/app/components/HealthEquity/index.tsx
+++ src/app/components/HealthEquity/index.tsx
@@ -1,24 +1,33 @@
+import Image from "next/image";
 import React from "react";
 
 export default function HealthEquity() {
   return (
-    <div className="section health">
+    <div className="health">
+      <Image
+        alt="Health Equity NY"
+        src="/images/health-equity.png"
+        width={200}
+        height={200}
+        className="health-img"
+      />
       <div className="health-content animated">
         <h6>HEALTH EQUITY NY</h6>
         <h4 className="font-bold health-content__title">
           Improving Health <br /> in Our Communities
         </h4>
         <p className="health-content__content">
-          SOMO&rsquo;s network provides culturally competent care to patients located in some of NYC&rsquo;s
-          most vulnerable communities and inmigrant neighborhoods of New York.
+          SOMO&rsquo;s network provides culturally competent care to patients
+          located in some of NYC&rsquo;s most vulnerable communities and
+          inmigrant neighborhoods of New York.
         </p>
         <div className="mt-5 flex justify-start items-center container-buttons__health">
-            <a href="" className="btn-health">
-                ANNUAL REPORT
-            </a>
-            <a href="" className="mx-3 btn-health__outset">
-                FIND A DOCTOR
-            </a>
+          <a href="" className="btn-health">
+            ANNUAL REPORT
+          </a>
+          <a href="" className="mx-3 btn-health__outset">
+            FIND A DOCTOR
+          </a>
         </div>
       </div>
     </div>
--- src/app/components/WhoWeAre/index.tsx
+++ src/app/components/WhoWeAre/index.tsx
@@ -1,37 +1,53 @@
-'use client';
+"use client";
 import useIntersectionObserver from "@/app/hooks/useIntersectionObserver";
 import { useRef } from "react";
 
 export default function WhoWeAre() {
   const whoWeAreRef = useRef(null);
-  const { show } = useIntersectionObserver(whoWeAreRef)
+  const { show } = useIntersectionObserver(whoWeAreRef);
 
   return (
-    <article className={`section about ${show ? 'animate-fade-up' : ''}`} ref={whoWeAreRef}>
-      <h3 className="title about-title">Who We Are</h3>
-      <p>
-        SOMOS Community Care (SOMOS), is New York’s only provider-led,
-        nonprofit, community-based integrated health care network. It is
-        comprised of over 2,500 network providers responsible for reaching and
-        delivering care to over 1 Million Medicaid lives citywide.
-      </p> <br />
-      <p>
-        Nearly one-quarter of New York City’s Medicaid population sees a SOMOS
-        network provider, who works almost exclusively in lower income
-        communities of color, in which health outcomes are lower and rates of
-        illness are higher - and where, until recently, many lacked access to
-        preventive care and had few options for care or were skeptical of the
-        corporate institutional options that did exist.
-      </p> <br />
-      <p>
-        SOMOS was designed as an intervention to turn around years of clinical
-        neglect, chronic lack of resources, and little engagement to bring about
-        healthier communities. For millions, a SOMOS network provider has meant
-        that hope has finally arrived.
-      </p>
-      <a href="" className="block w-max m-auto mt-5 btn-about">
-        <span>READ MORE</span>
-      </a>
-    </article>
+    <section
+      className={`section about [&>article]:m-4 ${show ? "animate-fade-up" : ""}`}
+      ref={whoWeAreRef}
+    >
+      <article>
+        <h3 className="title about-title">Who We Are</h3>
+        <p>
+          SOMOS Community Care (SOMOS), is New York’s only provider-led,
+          nonprofit, community-based integrated health care network. It is
+          comprised of over 2,500 network providers responsible for reaching and
+          delivering care to over 1 Million Medicaid lives citywide.
+        </p>{" "}
+        <br />
+        <p>
+          Nearly one-quarter of New York City’s Medicaid population sees a SOMOS
+          network provider, who works almost exclusively in lower income
+          communities of color, in which health outcomes are lower and rates of
+          illness are higher - and where, until recently, many lacked access to
+          preventive care and had few options for care or were skeptical of the
+          corporate institutional options that did exist.
+        </p>{" "}
+        <br />
+        <p>
+          SOMOS was designed as an intervention to turn around years of clinical
+          neglect, chronic lack of resources, and little engagement to bring
+          about healthier communities. For millions, a SOMOS network provider
+          has meant that hope has finally arrived.
+        </p>
+        <a href="" className="block w-max m-auto mt-5 btn-about">
+          <span>READ MORE</span>
+        </a>
+      </article>
+      <article>
+        <iframe
+          width="560"
+          height="315"
+          src="https://www.youtube.com/embed/RSgefnLznIY"
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        ></iframe>
+      </article>
+    </section>
   );
 }
