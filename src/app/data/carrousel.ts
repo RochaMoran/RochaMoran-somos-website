27c29f4bb5949f1554611e808dc275d2e26f7025
RochaMoran <rochamoran1@gmail.com>
2023-07-18 20:13:46 -0600

    FIX: colors and img carrousel

--- src/app/data/carrousel.ts
+++ src/app/data/carrousel.ts
@@ -1,18 +1,18 @@
-import img2 from '../../../public/carrousel/carrousel-2.webp';
+import img1 from '../../../public/carrousel/carrousel-1.svg';
 
 export const carrouselData = [
-  {
-    id: 1,
-    title: 'Health Care Revolution',
-    subtitle: 'WHERE INNOVATION MEETS COMPASSION IN A',
-    src: '/carrousel/carrousel-1.mp4',
-    isVideo: true
-  },
   // {
-  //   id: 2,
+  //   id: 1,
   //   title: 'Health Care Revolution',
-  //   subtitle: 'We Know how to take care of you',
-  //   src: img2,
-  //   isVideo: false
+  //   subtitle: 'WHERE INNOVATION MEETS COMPASSION IN A',
+  //   src: '/carrousel/carrousel-1.mp4',
+  //   isVideo: true
   // },
+  {
+    id: 2,
+    title: 'Health Care Revolution',
+    subtitle: 'We Know how to take care of you',
+    src: img1,
+    isVideo: false
+  },
 ];
